a15adb0c42bf56b638ba3c58578019ac47ee055d
nawin <150696567+n4win@users.noreply.github.com>
2026-05-11 14:55:13 +0700

    Update menu.ts

--- src/utils/constant/menu.ts
+++ src/utils/constant/menu.ts
@@ -132,7 +132,7 @@ export const Menu: AppMenu = {
       icon: Icon(IconUsers),
     },
     {
-      title: "ข้อมูลหน่วยงาน1",
+      title: "ข้อมูลหน่วยงาน",
       path: "/admin/department",
       disabled: true,
       icon: Icon(Icon123),
